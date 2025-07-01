55857b348dfa36b1f2517a8d707712c3811d93f6
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-07-01 17:55:19 +0300

    selection menu core

---
+++ Assets/Game/Scripts/CanvasBehavior.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasBehavior : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField input;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private TMP_Text minSliderValue;
+    [SerializeField] private TMP_Text maxSliderValue;
+    [SerializeField] private TMP_Text currentSliderValue;
+
+    void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { SliderUpdate(); });
+        SetSliderBounds(slider.minValue, slider.maxValue);
+    }
+
+    public void PrintInput()
+    {
+        Debug.Log("Пользователь ввел: \n" + "Name:" + input.text + "\nNumber: " + slider.value + "\nObject: " + dropdown.value);
+    }
+
+    public void SliderUpdate()
+    {
+        currentSliderValue.text = slider.value.ToString();
+    }
+
+    private void SetSliderBounds(float up, float down) // rename
+    {
+        minSliderValue.text = up.ToString();
+        maxSliderValue.text = down.ToString();
+    }
+}
