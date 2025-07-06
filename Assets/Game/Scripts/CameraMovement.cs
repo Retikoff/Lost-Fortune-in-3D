aebe1f4396b1a3e91ea9846a4ec4215597ef637f
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-07-06 17:00:31 +0300

    CameraMovement update

--- Assets/Game/Scripts/CameraMovement.cs
+++ Assets/Game/Scripts/CameraMovement.cs
@@ -1,23 +1,30 @@
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
     private CameraStates cameraState;
     [SerializeField] private Vector3[] positions;
+    [SerializeField] private Vector3[] rotations;
     private int currentPositionId = 0;
+    InputAction action;
     void Start()
     {
         cameraState = CameraStates.Empty;
+        action = InputSystem.actions.FindAction("Jump");
         MoveToNextPosition();
     }
 
     void Update()
     {
         //states?? i wanna keep them to separate dialogue logic and build it reliable on camerastates
-        if (Input.GetKeyDown(KeyCode.Space) && currentPositionId < positions.Length)
+
+        if (action.WasPressedThisFrame() && currentPositionId < positions.Length)
         {
             MoveToNextPosition();
+            UpdateState();
         }
     }
 
@@ -26,8 +33,33 @@ public class CameraMovement : MonoBehaviour
         this.cameraState = cameraState;
     }
 
-    private void MoveToNextPosition(float duration = 1f)
+    private void MoveToNextPosition(float duration = 2f)
     {
         transform.DOMove(positions[currentPositionId++], duration);
+    }
+
+    private void UpdateState()
+    {
+        switch (currentPositionId)
+        {
+            case 0:
+                cameraState = CameraStates.Empty;
+                break;
+            case 1:
+                cameraState = CameraStates.Init;
+                break;
+            case 2:
+                cameraState = CameraStates.NPC;
+                break;
+            case 3:
+                cameraState = CameraStates.Forest;
+                break;
+            case 4:
+                cameraState = CameraStates.Lawn;
+                break;
+            default:
+                cameraState = CameraStates.Error;
+                break;
+        }
     }
 }
--- Assets/Game/Scripts/CameraStates.cs
+++ Assets/Game/Scripts/CameraStates.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public enum CameraStates
 {
+    Error,
     Empty,
     Init,
     NPC,
